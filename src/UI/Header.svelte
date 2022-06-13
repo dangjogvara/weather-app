018f8d870c4451dd5730d10a12212e8b46492a49
Dan Poulsen <dangjogvara@gmail.com>
2022-06-13 21:16:44 +0100

    first commit

---
+++ src/UI/Header.svelte
@@ -0,0 +1,12 @@
+<!-- Navbar -->
+<nav class="p-6 container bg-red-400 mx-auto relative">
+  <!-- Flex Container -->
+  <div class="flex container justify-between">
+    <div>Weather App</div>
+    <div class="space-x-2">
+      <a href="#" class="">Login</a>
+      <a href="#">Contact</a>
+      <a href="#">About Us</a>
+    </div>
+  </div>
+</nav>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {
+      colors: {
+        BackGroundMainPage: 'rgb(15 23 42/var(--tw-bg-opacity))',
+      },
+    },
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
